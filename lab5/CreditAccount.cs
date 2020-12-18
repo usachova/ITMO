69174af5713b8f532e9aad339fd8a4438b912104
usachova <55155963+usachova@users.noreply.github.com>
2020-12-18 20:50:14 +0300

    Add files via upload

---
+++ lab5/CreditAccount.cs
@@ -0,0 +1,62 @@
+using System;
+
+namespace lab5
+{
+    public class CreditAccount : Account
+    {
+        public class CreditParameters : Parameters
+        {
+            public readonly int _limit;
+            public readonly int _commission;
+
+            public CreditParameters(int limit, int commission)
+            {
+                _limit = limit;
+                _commission = commission;
+            }
+        }
+        
+        private CreditParameters _parameters;
+        public CreditAccount(CreditParameters parameters, DateTime date, int sum)
+        {
+            _type = AccountType.credit;
+            _date = date;
+            _parameters = parameters;
+            _sum = sum;
+        }
+
+        public override void Withdrawal(int value)
+        {
+            int commission = (_sum - value < 0) ? _parameters._commission : 0;
+            if (_sum + _parameters._limit >= value + commission)
+            {
+                _sum -= value + commission;
+                Console.WriteLine("Вы успешно сняли " + value + " рублей. Комиссия составила " + commission + " рублей.");
+            }
+            else
+            {
+                Console.WriteLine("Недостаточно средств.");
+            }
+        }
+
+        public override void Transfer(int value, Client client)
+        {
+            int commission = (_sum - value < 0) ? _parameters._commission : 0;
+            int rand = (new Random()).Next(0, 1);
+            if (client.TryToTransfer(value) && rand == 1 && _sum + _parameters._limit >= value + commission)
+            {
+                _sum -= value + commission;
+                Console.WriteLine("Вы успешно перевели деньги на счёт " + client.GetName() + " " + client.GetSurname() + ".");
+            }
+            else
+            {
+                Console.WriteLine("Перевод отклонён");
+            }
+        }
+
+        public override void MoveInTime()
+        {
+            _date += TimeSpan.FromDays(1);
+        }
+    }
+}
---
+++ lab5/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace lab5
+{
+    class Program
+    {
+        private static Client _getClient;
+
+        static void Main(string[] args)
+        {
+            Manager.AddBank("Сбербанк", new DebitAccount.DebitParameters(4), 
+                                      new DepositAccount.DepositParameters(DateTime.Today + TimeSpan.FromDays(100), 5000, 10000, 3, 4, 5), 
+                                      new CreditAccount.CreditParameters(30000, 50), 1000);
+            Bank bank = Manager.GetBank(0);
+            bank.AddClient("Дим", "Димыч");
+            _getClient = bank.GetClient(0);
+            Client sliceofkekus = _getClient;
+            sliceofkekus.AddAddress("Тачка");
+            Console.WriteLine(sliceofkekus.GetAddress());
+            bank.AddClient("Данька", "Наш").AddPassport("1234560");
+            _getClient = bank.GetClient(1);
+            Client danyaffff = _getClient;
+            sliceofkekus.AddAccount(Account.AccountType.debit, bank.GetDebitParameters(), 0);
+            sliceofkekus.Replenishment(2000, Account.AccountType.debit);
+            danyaffff.AddAccount(Account.AccountType.credit, bank.GetDCreditParameters(), 1000);
+            sliceofkekus.Withdrawal(1500, Account.AccountType.debit);
+            sliceofkekus.Transfer(500, Account.AccountType.debit, danyaffff);
+        }
+    }
+}  
